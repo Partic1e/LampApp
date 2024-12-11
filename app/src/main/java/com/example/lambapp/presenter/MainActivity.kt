b940ebe4e958670ed28abd0873251ac2f7e7e74a
Igor <IgorLom2003@mail.ru>
2024-12-11 16:24:38 +0500

    create rep

---
+++ app/src/main/java/com/example/lambapp/presenter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lambapp.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lambapp.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
